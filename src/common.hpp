cf3889e9ac9400798ec77b93e2b52c508193ac52
InKryption <InKryption07@gmail.com>
2021-01-10 13:55:30 +0100

    Cleaned up some redundancy and added a comment

--- src/common.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __INK__COMMON_INCLUDES__HEADER_FILE_
-#define __INK__COMMON_INCLUDES__HEADER_FILE_
-
-#include <string>
-#include <vector>
-#include <unordered_map>
-
-#endif
--- src/main.hpp
+++ src/main.hpp
@@ -1,1 +1,2 @@
-#include "common.hpp"
+// Do not include this anywhere but the main entry point source file. It should only be used to make sure there's no excessive #include junk at the start
+#include <stdio.h>
