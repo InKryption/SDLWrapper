735f5c42f2ed245172c9ed95ab568cdb227433cd
InKryption <InKryption07@gmail.com>
2021-01-13 22:19:48 +0100

    I always forget that the .make folder doesn't add automatically

---
+++ .make/Debug.mk
@@ -0,0 +1,13 @@
+
+COMPILER:=x86_64-w64-mingw32-g++-posix
+ROOT:=$(CURDIR)
+BIN:=bin
+SRC:=src
+BUILD:=Debug
+NAME:=Project
+
+INCLUDE:=
+LIBRARY:=
+LIBNAME:=
+LFLAG:= -static
+CFLAG:= -Wall -Wpedantic -Werror -Og
