57231838a56280e7ed226de0bf685447137c86a5
InKryption <inkryption07@gmail.com>
2021-01-15 08:10:12 +0000

    Demonstration of Unique Object Runtime ID

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include "main.hpp"
 
+typedef const bool* const TypeID;
+template<typename> TypeID TypeIdOf() noexcept {
+	static const bool idLoc(0);
+	return &idLoc;
+}
 
+class Base {
+	public:
+	virtual TypeID Type() const noexcept = 0;
+	template<typename T> const bool IsType() const { return Type() == TypeIdOf<T>(); }
+};
+
+class Derived1: public Base {
+	public: TypeID Type() const noexcept override { return TypeIdOf<Derived1>(); }
+};
+
+class Derived2: public Base {
+	public: TypeID Type() const noexcept override { return TypeIdOf<Derived2>(); }
+};
 
 int main(int argc, char* argv[])
 {
-	
-	
-	
+	Base* b0 = new Base;
+	Base* b1 = new Derived1;
+	Base* b2 = new Derived2;
+	printf("%i\n", b1->IsType<Derived2>());
 	return 0;
 }
