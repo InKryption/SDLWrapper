99c319e0602ebcf245addf50c1885dc60aa67a56
InK <InKryption07@gmail.com>
2021-04-03 01:43:42 +0200

    Test for demonstration that everything is functioning as intended

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -6,12 +6,27 @@
 #include "SDLWrapper.hpp"
 
 int main() {
+	
+	using namespace ink::SDL::internal;
+	
 	ink::SDL::internal::SDL_Init(ink::SDL::internal::INIT_EVERYTHING);
-	auto wnd = ink::SDL::internal::SDL_CreateWindow("", 100, 100, 100, 100, ink::SDL::internal::SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+	auto wnd = ink::SDL::internal::SDL_CreateWindow("", 100, 100, 100, 100, ink::SDL::internal::SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
 	
-	std::this_thread::sleep_for(std::chrono::seconds(2));
+	bool running = true;
 	
-	ink::SDL::internal::SDL_DestroyWindow(wnd);
+	SDL_Event e;
+	while (running) {
+		while (SDL_PollEvent(&e) != 0) switch(e.type) {
+			case SDL_QUIT:
+				running = false;
+			break;
+		}
+		
+		
+	}
+	
+	SDL_DestroyWindow(wnd);
+	SDL_Quit();
 	
 	return 0;
 }
