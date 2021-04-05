dffe7b1cf41f7d48921b31c1075a659b37c14dc9
InK <InKryption07@gmail.com>
2021-04-05 15:45:03 +0200

    Developing the Frame class

--- src/Frame.hpp
+++ src/Frame.hpp
@@ -38,46 +38,90 @@ namespace ink::SDL {
 		Frame()
 		{}
 		
-		public: [[nodiscard]]
-		Frame(std::string_view title, int w, int h, FlagCtr const& flag_struct, int x = internal::WINDOWPOS_UNDEFINED, int y = internal::WINDOWPOS_UNDEFINED, int render_driver = -1)
-		{ init(title.cbegin(), x, y, w, h, flag_struct.wnd_flag, render_driver, flag_struct.rnd_flag); }
-		
-		
-		// Move will throw if acting as assignment to initialized Frame.
-		public: void
-		operator=(Frame && rval)
-		{
-			if (M_data != nullptr) throw;
-			std::swap(M_data, rval.M_data);
-		}
-		
 		public:
 		Frame(Frame const&) = delete;
 		
-		private: void init(char const* title, int x, int y, int w, int h, ink::SDL::internal::Uint32 wnd_flags, int render_driver, ink::SDL::internal::Uint32 rnd_flags) {
-			
-			M_data = new impl;
-			M_data->evt = internal::SDL_Event{};
-			M_data->wnd = internal::SDL_CreateWindow(title, x, y, w, h, wnd_flags);
-			M_data->rnd = internal::SDL_CreateRenderer(M_data->wnd, render_driver, rnd_flags);
-			
+		public: [[nodiscard]]
+		Frame(std::string_view title, int w, int h, FlagCtr const& flag_struct, int x = internal::WINDOWPOS_UNDEFINED, int y = internal::WINDOWPOS_UNDEFINED, int render_driver = -1)
+		{
+			( M_data = new impl{} )
+				->init(title.cbegin(), x, y, w, h, flag_struct.wnd_flag, render_driver, flag_struct.rnd_flag);
 		}
 		
-		private: void deinit() {
-			
-			internal::SDL_DestroyRenderer(M_data->rnd);
-			internal::SDL_DestroyWindow(M_data->wnd);
-			delete M_data;
-			
+		public:
+		~Frame()
+		{ delete M_data; M_data = nullptr; }
+		
+		
+		
+		// Move will throw if acting as assignment to already-initialized Frame.
+		public: void
+		operator=(Frame && rval) try
+		{
+			if (M_data != nullptr) throw "Encountered assignment to already-initiaized Frame object. Throwing intentionally.";
+			std::swap(M_data, rval.M_data);
 		}
+		catch(const char* msg) {
+			puts(msg);
+			std::exit(EXIT_FAILURE);
+		}
+		
+		
+		
+		// Return a raw pointer to the SDL_Window
+		public: constexpr auto*
+		window() {
+			return M_data->wnd;
+		}
+		
+		// Return a raw pointer to the SDL_Renderer
+		public: constexpr auto*
+		renderer() {
+			return M_data->rnd;
+		}
+		
+		
+		
+		// Return a constant reference to the event structure
+		public: constexpr const auto&
+		event() {
+			return M_data->evt;
+		}
+		
+		// Polls the next queued event, and returns true if there is another pending event to poll.
+		public: bool
+		poll_events() {
+			return internal::SDL_PollEvent( &M_data->evt ) != 0;
+		}
+		
+		
+		
+		public: 
+		
+		
 		
 		private: struct impl {
 			
 			internal::SDL_Event evt{};
 			internal::SDL_Window* wnd{nullptr};
 			internal::SDL_Renderer* rnd{nullptr};
-			constexpr auto active() const
-			{ return ( wnd != nullptr ) && ( rnd != nullptr ); }
+			
+			void init(	char const* title,
+						int x, int y, int w, int h,
+						ink::SDL::internal::Uint32 wnd_flags,
+						int render_driver,
+						ink::SDL::internal::Uint32 rnd_flags) {
+				
+				evt = internal::SDL_Event{};
+				wnd = internal::SDL_CreateWindow(title, x, y, w, h, wnd_flags);
+				rnd = internal::SDL_CreateRenderer(wnd, render_driver, rnd_flags);
+				
+			}
+			
+			void deinit() {
+				internal::SDL_DestroyRenderer(rnd);
+				internal::SDL_DestroyWindow(wnd);
+			}
 			
 		}* M_data{nullptr};
 		
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -2,14 +2,10 @@
 #include <thread>
 #include <stdio.h>
 #include <type_traits>
+#include <cstring>
 
 #define SDL_MAIN_HANDLED
 #include "SDLWrapper.hpp"
-
-template<typename T> struct rtup_impl;
-template<typename T> static constexpr auto rtup = rtup_impl<T>::value;
-
-template<template<auto...> typename T, auto... v> struct rtup_impl<T<v...>> { static constexpr auto value = std::make_tuple(v...); };
 
 namespace SDL = ink::SDL;
 
@@ -21,20 +17,24 @@ int main() {
 		.IMG_png()
 		.init();
 	
-	auto wnd = SDL_CreateWindow("", 100, 100, 100, 100, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+	SDL::Frame frame; // Allows for safe forward declaration.
+	frame = SDL::Frame("Title", 100, 100, SDL::Frame::FlagCtr{}.RESIZABLE());
+	
+	//auto wnd = SDL_CreateWindow("", 100, 100, 100, 100, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
 	
 	bool running = true;
 	
-	SDL_Event e;
 	while (running) {
-		while (SDL_PollEvent(&e) != 0) switch(e.type) {
+		
+		while (frame.poll_events()) switch(frame.event().type) {
 			case SDL_QUIT:
 				running = false;
 			break;
 		}
+		
 	}
 	
-	SDL_DestroyWindow(wnd);
+	
 	
 	return 0;
 }
